f0f04c2300ff3b56c890d37628a1499e53b66e29
kento-koyama <76421729+kento-koyama@users.noreply.github.com>
2024-09-27 16:02:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# 日本語フォントを設定
+font_path = '/usr/share/fonts/truetype/fonts-japanese-gothic.ttf'  # IPAexGothicのパスを指定
+font_prop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
 
 # Streamlitアプリのタイトル
 st.title('バクテリアデータ可視化アプリ')
@@ -8,8 +14,8 @@ st.title('バクテリアデータ可視化アプリ')
 # CSVファイルのURL
 csv_url = "https://raw.githubusercontent.com/kento-koyama/food_micro_data_risk/main/%E9%A3%9F%E4%B8%AD%E6%AF%92%E7%B4%B0%E8%8F%8C%E6%B1%9A%E6%9F%93%E5%AE%9F%E6%85%8B_%E6%B1%9A%E6%9F%93%E7%8E%87.csv"
 
-# データの読み込み
-df = pd.read_csv(csv_url)
+# データの読み込み (エンコーディングを指定)
+df = pd.read_csv(csv_url, encoding='utf-8')
 
 # 欠損値の削除
 df = df.dropna(how='any', axis=0)
@@ -26,8 +32,8 @@ st.write(bacteria_counts)
 st.write("バクテリアのカウント数のグラフ")
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.barh(bacteria_counts['バクテリア名'], bacteria_counts['カウント数'], color='skyblue')
-ax.set_xlabel('カウント数')
-ax.set_ylabel('バクテリア名')
-ax.set_title('バクテリアのカウント数')
+ax.set_xlabel('カウント数', fontproperties=font_prop)
+ax.set_ylabel('バクテリア名', fontproperties=font_prop)
+ax.set_title('バクテリアのカウント数', fontproperties=font_prop)
 ax.invert_yaxis()  # バーを上から降順に表示
 st.pyplot(fig)
