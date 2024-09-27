bff151dc274d7ce4a0a10351ca61f2424aa6b1e2
kento-koyama <76421729+kento-koyama@users.noreply.github.com>
2024-09-27 15:55:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from pygwalker.api.streamlit import StreamlitRenderer
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# URL of the CSV file
-#csv_url = "https://raw.githubusercontent.com/kento-koyama/food_micro_data_risk/main/%E9%A3%9F%E4%B8%AD%E6%AF%92%E7%B4%B0%E8%8F%8C%E6%B1%9A%E6%9F%93%E5%AE%9F%E6%85%8B_%E6%B1%9A%E6%9F%93%E6%BF%83%E5%BA%A6.csv"
+# Streamlitアプリのタイトル
+st.title('バクテリアデータ可視化アプリ')
+
+# CSVファイルのURL
 csv_url = "https://raw.githubusercontent.com/kento-koyama/food_micro_data_risk/main/%E9%A3%9F%E4%B8%AD%E6%AF%92%E7%B4%B0%E8%8F%8C%E6%B1%9A%E6%9F%93%E5%AE%9F%E6%85%8B_%E6%B1%9A%E6%9F%93%E7%8E%87.csv"
 
-# Adjust the width of the Streamlit page
-st.set_page_config(
-    page_title="Use Pygwalker In Streamlit",
-    layout="wide"
-)
-# Import your data
-def load_data(url):
-    return pd.read_csv(url)
-df = load_data(csv_url) 
-pyg_app = StreamlitRenderer(df)
- 
-pyg_app.explorer()
- 
+# データの読み込み
+df = pd.read_csv(csv_url)
+
+# 欠損値の削除
+df = df.dropna(how='any', axis=0)
+
+# バクテリア名のカウント
+bacteria_counts = df['Bacteria'].value_counts().reset_index()
+bacteria_counts.columns = ['バクテリア名', 'カウント数']
+
+# テーブルの表示
+st.write("バクテリア名とカウント数のテーブル")
+st.write(bacteria_counts)
+
+# バクテリアカウントをグラフで可視化
+st.write("バクテリアのカウント数のグラフ")
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.barh(bacteria_counts['バクテリア名'], bacteria_counts['カウント数'], color='skyblue')
+ax.set_xlabel('カウント数')
+ax.set_ylabel('バクテリア名')
+ax.set_title('バクテリアのカウント数')
+ax.invert_yaxis()  # バーを上から降順に表示
+st.pyplot(fig)
