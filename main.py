137ae827a2a0855e6dff4a0123d92f19d217c718
kento-koyama <76421729+kento-koyama@users.noreply.github.com>
2024-09-29 06:25:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ font_path = 'NotoSansCJKjp-Regular.otf'  # プロジェクトディレクトリ�
 # Streamlit のアプリケーション
 st.title('食中毒細菌の陽性/陰性の検査の統計まとめ')
 st.write('食中毒細菌汚染実態_汚染率.csvの可視化です。')
+st.write('-----------')
 # フォントの設定
 fm.fontManager.addfont(font_path)
 font_prop = fm.FontProperties(fname=font_path)
